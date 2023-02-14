c37a6d11ca89560e73d6ae4ba0fe0837f1306cb3
DIMMOY <dimmy6789@gmail.com>
2023-02-14 20:57:21 +0700

    [Minor Fixed] Change Function Name

--- src/middleware/isAdmin.middleware.ts
+++ src/middleware/isAdmin.middleware.ts
@@ -39,7 +39,7 @@ export class IsAdminMiddleware implements NestMiddleware {
         return response.status(userData.statusCode).send(userData);
 
       // IsAdmin
-      const userHasRoleData = await this.userHasRoleService.find({
+      const userHasRoleData = await this.userHasRoleService.list({
         userId: userData.data._id,
         role: 2,
         deletedAt: null,
--- src/middleware/isAdminOrAdvisor.middleware.ts
+++ src/middleware/isAdminOrAdvisor.middleware.ts
@@ -39,7 +39,7 @@ export class IsAdminOrAdvisorMiddleware implements NestMiddleware {
         return response.status(userData.statusCode).send(userData);
 
       // Is Admin Or Advisor
-      const userHasRoleData = await this.userHasRoleService.find({
+      const userHasRoleData = await this.userHasRoleService.list({
         userId: userData.data._id,
         role: { $in: [1, 2] },
         deletedAt: null,
