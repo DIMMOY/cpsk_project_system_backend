e8d19a1ec33ee3939305b58c310931ac264a1871
DIMMOY <dimmy6789@gmail.com>
2023-03-15 05:42:29 +0700

    [Deleted] Delete Auth File

--- src/modules/auth.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from 'src/services/auth.service';
-@Module({
-  imports: [],
-  providers: [AuthService],
-})
-export class AuthModule {}
